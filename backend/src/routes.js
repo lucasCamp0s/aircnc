4975f34f28f0c56efb61b5e3787bde2c179dfac5
Lucas Campos <lucas.campos.silva85@gmai.com>
2019-10-13 11:36:55 -0300

    back-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,17 @@
 const express = require('express');
-
+const multer = require('multer');
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashBoardController = require('./controllers/DashboardController');
+const uploadConfig = require('./config/upload');
+const BookingController = require('./controllers/BookingController');
 const routes = express.Router();
+const upload = multer(uploadConfig);
 
-routes.post('/users', (req, res) => {
-    return res.json(req.body);
-});
-
-module.exports = routes;
+routes.post('/sessions', SessionController.store);
+routes.get('/Users',SessionController.show);
+routes.get('/spots',SpotController.index);
+routes.get('/dashboard',DashBoardController.show);
+routes.post('/spot',upload.single('thumbnail') , SpotController.store);
+routes.post('/spots/:spot_id/bookings',BookingController.store);
+module.exports = routes;  

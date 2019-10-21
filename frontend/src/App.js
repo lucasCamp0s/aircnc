76ef298d313fd556d5cb4d6fe49aee65fda95c2a
LUCAS CAMPOS DA SILVA <u18321@academico.cotuca.unicamp.br>
2019-10-21 17:55:01 -0300

    front

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-
+import React,{useState} from 'react';
+import api from './services/api';
 import './App.css';
 import logo from './assets/logo.svg'
 function App() {
+  const [email,setEmail] = useState('');
+  
+  async function handleSubmit(event){
+    event.preventDefault();
+    const response = await api.post('/sessions', {
+    email:email,
+    });
+    const { _id } = response.data;
+    //console.log(_id);
+    localStorage.setItem('user',_id);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnc" className="logo" />
       <div className="content">
         <p>Ofereça <strong>spots</strong> para programadores encontre <strong>talentos</strong> para sua empresa</p>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input
             id="email"
             type="email"
             placeholder="seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
           />
           <button className="btn" type="submit">Entrar</button>
         </form>

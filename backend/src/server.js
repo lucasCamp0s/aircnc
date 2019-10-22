c0c3a3b1a84d12a2a8f63f2a10a45293b3fedd5c
Lucas Campos <lucas.campos.silva85@gmai.com>
2019-10-22 00:06:51 -0300

    final front

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const routes = require('./routes');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://teste:teste@cluster0-j66pd.mongodb.net/test?retr
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname,'..','uploads')));
 app.use(routes);
 
 app.listen(3000);
